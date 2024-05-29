2a1c0426bcd70554f64cc7901fd525224353be1f
rGuszz <gusrodriguezayala@gmail.com>
2024-05-29 05:01:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ opciones = st.container()
 with titulo:
         st.title("Bienvenido a la interfaz para calcular el precio de opciones y ver otros datos")
 with datos:
-        wb = xw.Book(r"")
+        wb = xw.Book(r"Cálculos Seguro Por si las Flies M.xlsm")
         sheet_prima = wb.sheets("Cálculo de prima")
         sheet_resumen = wb.sheets("Resumen")
-        edad = int(input("Elije la edad del asegurado"))
+        edad = st.slider("Elije la edad del asegurado", min_value=30, max_value=45, step=1, value=30)
         sheet_prima.range(11,13).value = edad
         macro_calc = wb.macro("Resumen")
         macro_calc()
         df = sheet_resumen.range("B8:E27").options(pd.DataFrame, header=1, index=False, expand = "table").value
         wb.save()
+        st.write(df)
